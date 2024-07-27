811e8ddfd05eeda66b8f604ac1ba6be29f92156d
Renuka Prasad <rprasad@axiumplastics.com>
2024-07-27 03:21:49 +0000

    filtering

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
-    '../model/formatter'
-], function(Controller, JSONModel, fmtr) {
+    '../model/formatter',
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, fmtr, Filter, FilterOperator) {
     'use strict';
      return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
         formatter: fmtr,
@@ -11,6 +13,16 @@ sap.ui.define([
                 "currency": "EUR"
             });
             this.getView().setModel(cmodel, "view");
+        },
+        onFilterInvoices(e){
+            const fl = [];
+            const skey = e.getParameter("query");
+            if(skey){
+                fl.push(new Filter("ProductName", FilterOperator.Contains, skey));
+            } 
+            const ilist = this.byId("invoiceList");
+            const litems = ilist.getBinding("items");
+            litems.filter(fl);
         }
      })
 });
